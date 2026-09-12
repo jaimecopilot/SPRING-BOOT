05f55d99dd30debe8632325addb0dec9c0dfd05f
jaimecopilot <jaimegithub@outlook.com>
2026-09-13 00:39:28 +0200

    feat(m1): añade SolicitanteDTO anidado de 1.3

---
+++ M1/proyecto/src/main/java/es/mecd/demo/miproyecto/dto/SolicitanteDTO.java
@@ -0,0 +1,45 @@
+package es.mecd.demo.miproyecto.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class SolicitanteDTO {
+
+    private String nombre;
+    private String apellidos;
+
+    @JsonProperty("dni")
+    private String documentoIdentidad;
+
+    public SolicitanteDTO() {
+    }
+
+    public SolicitanteDTO(String nombre, String apellidos, String documentoIdentidad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.documentoIdentidad = documentoIdentidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDocumentoIdentidad() {
+        return documentoIdentidad;
+    }
+
+    public void setDocumentoIdentidad(String documentoIdentidad) {
+        this.documentoIdentidad = documentoIdentidad;
+    }
+}
